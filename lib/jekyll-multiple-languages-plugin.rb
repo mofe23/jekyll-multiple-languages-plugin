1b5b58d62583daa7254a8d259c7f0a0ad56d34fa
Moritz Federspiel <moritz.federspiel@steadysense.at>
2021-05-04 11:31:08 +0200

    Catch exception, log errors

--- lib/jekyll-multiple-languages-plugin.rb
+++ lib/jekyll-multiple-languages-plugin.rb
@@ -556,18 +556,23 @@ unless Hash.method_defined? :access
     #======================================
     def access(path)
       ret = self
-      
-      path.split('.').each do |p|
-      
-        if p.to_i.to_s == p
-          ret = ret[p.to_i]
-        else
-          ret = ret[p.to_s] || ret[p.to_sym]
+
+      begin
+        path.split('.').each do |p|
+
+          if p.to_i.to_s == p
+            ret = ret[p.to_i]
+          else
+            ret = ret[p.to_s] || ret[p.to_sym]
+          end
+
+          break unless ret
         end
-        
-        break unless ret
+      rescue TypeError => e
+        puts("Could not find key '#{path}', aborting. . .")
+        raise
+
       end
-      
       ret
     end
   end
